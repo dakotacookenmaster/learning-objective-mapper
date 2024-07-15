c9b4348c16fcf803fb9e3491fd46430f9c7cac2b
Dakota Cookenmaster <dakotacookenmaster@southern.edu>
2024-07-15 14:13:44 -0400

    Commented out file for linting (first deploy)

--- src/api-faker/fetchGetCourses.ts
+++ src/api-faker/fetchGetCourses.ts
@@ -1,6 +1,6 @@
-import { faker } from "@faker-js/faker"
+// import { faker } from "@faker-js/faker"
 
-const fetchGetCourses = () => {
-    const courseNames = [...Array(32).map(() => faker.company.name())]
-    const objectives = [...Array(32).map(() => faker.company.buzzVerb())]
-}
+// const fetchGetCourses = () => {
+//     const courseNames = [...Array(32).map(() => faker.company.name())]
+//     const objectives = [...Array(32).map(() => faker.company.buzzVerb())]
+// }
